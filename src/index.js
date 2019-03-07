398a2392d8f5ceb5ba267f3e97bdc09eaa65b13f
Tyler Krupicka <Tyler_Krupicka@intuit.com>
2019-03-07 09:42:31 -0800

    get it working with peer dependency and types

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const AxeBuilder = require('axe-webdriverjs');
-const bindings = require('./bindings');
+const AxeBuilder = require("axe-webdriverjs");
+const bindings = require("./bindings");
 
 class AxeWebDriverIOBuilder extends AxeBuilder {
   constructor(driver, source) {
